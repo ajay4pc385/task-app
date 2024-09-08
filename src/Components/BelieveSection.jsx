2370b153659c5934b7790bb6e0d9bcde99ac24db
ajay4pc385 <ajaykalash385>
2024-09-08 09:50:16 +0530

    first commit

--- src/Components/BelieveSection.jsx
+++ src/Components/BelieveSection.jsx
@@ -5,7 +5,7 @@ import Img from '../image/family-showcasing-their-home-scaled.jpg'; // Ensure th
 const BeliefSection = () => {
   return (
     <>
-      <section className="bg-gray-900 text-white py-12">
+      <section className="container bg-gray-900 text-white py-12">
         <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
           {/* Left Side */}
           <div className="md:w-1/2 mb-8 md:mb-0">
@@ -20,22 +20,19 @@ const BeliefSection = () => {
               height: '90vh', // Adjust height as needed
               width: '100%',
               maxWidth: '500px', // Adjust max-width if needed
-              position:"relative",
-              left:"224px",
-            
-              top:"-48px"
+        
 
             }}
           ></div>
         </div>
 
         {/* Bottom Statistics */}
-        <div className="container mx-auto px-4 mt-8 flex flex-wrap justify-end gap-4 " style={{position:"relative" , top:"-170px" , left:"50px " }}>
+        <div className="container mx-auto px-4 mt-8 flex flex-wrap justify-end gap-4 " >
           <div className="bg-white text-gray-900 p-4 rounded-lg text-center w-1/4 md:w-1/6 hero-section">
             <h3 className="text-3xl font-bold">320+</h3>
             <p>Lenders</p>
           </div>
-          <div className="bg-white text-gray-900 p-4 rounded-lg text-center w-1/4 md:w-1/6 hero-section" style={{position:"relative" , top:"-10px" , }}>
+          <div className="bg-white text-gray-900 p-4 rounded-lg text-center w-1/4 md:w-1/6 hero-section" >
             <h3 className="text-3xl font-bold">20+</h3>
             <p>Years Experience</p>
           </div>
